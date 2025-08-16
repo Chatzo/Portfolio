41c1a10dc7490098c1e320793b0612e6990c4b3a
Chatzo <chatzoglakis@gmail.com>
2025-08-16 17:39:14 +0200

    Create a basic page with theme, menu and small text animations

---
+++ Portfolio/Portfolio/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const headline = document.querySelector('#typed_headline');
+    if (headline) {
+        function startTypedSequence() {
+            new Typed('#typed_headline', {
+                strings: ["<span class='fs-6'>Hi there! I'm</span><span class='fs-3 fw-bold ms-2 shining-text'>Georgios Chatzoglakis</span>"],
+                typeSpeed: 35,
+                showCursor: false,
+                onComplete: function () {
+                    new Typed('#typed_description', {
+                        strings: ["<p>I'm a <span class='shining-underline'>developer</span> based in Stockholm!</p>"],
+                        typeSpeed: 35,
+                        showCursor: false,
+
+                        onComplete: function () {
+                            setTimeout(() => {
+                                document.getElementById('typed_headline').innerHTML = '';
+                                document.getElementById('typed_description').innerHTML = '';
+                                startTypedSequence();
+                            }, 2000);
+                        }
+                    });
+                }
+            });
+        }
+
+        startTypedSequence();
+    }
+});
